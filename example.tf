a7a546f6e36908bc1281e960a4338cb8139ce272
Luis Angarita <luis.angarita@telos.com>
2020-05-28 17:20:01 -0400

    testing terraform cloud

---
+++ example.tf
@@ -0,0 +1,28 @@
+provider "aws" {
+  profile    = "default"
+  region     = var.region
+}
+
+resource "aws_instance" "example" {
+  ami             = "ami-b374d5a5"
+  instance_type   = "t2.micro"
+}
+
+resource "aws_eip" "ip" {
+    vpc = true
+    instance = aws_instance.example.id
+}
+
+output "ip" {
+  value = aws_eip.ip.public_ip
+}
+
+terraform {
+  backend "remote" {
+    organization = "techlu"
+
+    workspaces {
+      name = "dev-terraform"
+    }
+  }
+}
